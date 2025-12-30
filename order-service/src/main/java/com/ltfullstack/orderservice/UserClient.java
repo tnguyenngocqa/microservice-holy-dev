8cb6ab783df77ca7d671ce7f310c9d9c8906e770
tnguyenngocqa <tnguyenngocqa@gmail.com>
2025-12-30 21:59:11 +0700

    OpenFeign
    OpenFeign

---
+++ order-service/src/main/java/com/ltfullstack/orderservice/UserClient.java
@@ -0,0 +1,12 @@
+package com.ltfullstack.orderservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "user-service")
+public interface UserClient {
+
+    @GetMapping("/api/users/{id}")
+    UserDto getUserById(@PathVariable("id") Long id);
+}
